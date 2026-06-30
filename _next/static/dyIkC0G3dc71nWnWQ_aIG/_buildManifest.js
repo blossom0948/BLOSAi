09ee65dd549e52e4385d473d59b2de4b92d6377f
정우성 <blossom0948@gmail.com>
2026-07-01 01:22:16 +0900

    Deploy BLOS AI static site

---
+++ _next/static/dyIkC0G3dc71nWnWQ_aIG/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/BLOSAi//_next/:path+",
+        "destination": "/BLOSAi/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
